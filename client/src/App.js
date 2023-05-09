f92535557041b58eeaf855f80f025667ab16aadf
N3EMESIS <idiartlucas@gmail.com>
2023-05-09 13:08:28 -0300

    NavBar hecha, estilos basicos

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 // import { useEffect, useState } from 'react';
 import { HomePage, LandingPage, Detail, Cart } from "./Views/index";
+import { NavBar } from "./Components/index";
 
 function App() {
 
+  //************************************* OCULTAR / MOSTRAR NAVBAR *********************************/
+  const location = useLocation();
+  const showNav = location.pathname !== "/";
+
   return (
     <div className="App">
+      {showNav && <NavBar />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/Home" element={<HomePage />} />
         <Route path="/Detail" element={<Detail />} />
         <Route path="/Cart" element={<Cart />} />
       </Routes>
