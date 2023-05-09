63450b8a593c7c59301e6220382d42b12c2f799e
N3EMESIS <idiartlucas@gmail.com>
2023-05-09 13:24:21 -0300

    Algunos actions de redux hechos

---
+++ client/src/redux/actions/actions.js
@@ -0,0 +1,23 @@
+import { 
+    ADD_CART,
+    FILTER_NAME_PRODUCT, 
+    FILTER_PRICE_PRODUCT 
+} from "./Types/Types";
+
+// import axios from "axios";
+// const URL = "hhtp://localhost:3001";
+
+export const addCartProduct = (product) => ({
+    type: ADD_CART,
+    payload: product
+});
+
+export const filterNameProduct = (product) => ({
+    type: FILTER_NAME_PRODUCT,
+    payload: product
+});
+
+export const filterPriceProduct = (product) => ({
+    type: FILTER_PRICE_PRODUCT,
+    payload: product
+});
