0534ae46e3435e0544071da4acd7f053f9fd8eac
N3EMESIS <idiartlucas@gmail.com>
2023-05-09 12:16:14 -0300

    creadas las rutas principales

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+// import { useEffect, useState } from 'react';
+// import axios from "axios";
+import LandingPage from "./Views/LandingPage/LandingPage";
+import HomePage from './Views/HomePage/HomePage';
 
 function App() {
+
   return (
     <div className="App">
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </div>
   );
