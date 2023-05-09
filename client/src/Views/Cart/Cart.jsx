58bd6dd40dc0ce00729272516c0461a3779c75cb
N3EMESIS <idiartlucas@gmail.com>
2023-05-09 13:34:14 -0300

    Referencia del carrito hecha

--- client/src/Views/Cart/Cart.jsx
+++ client/src/Views/Cart/Cart.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import styles from "./Cart.module.css";
 
 function Cart() {
-    return (
-        <div>
+    const cart = useSelector((state) => state.cart);
 
+    return (
+        <div className={styles.mainContainer}>
+            <h1 className={styles.tittle} >Cart</h1>
+            {cart.length > 0 ? (
+                <div className={styles.container}>
+                    {cart.map(product => {
+                        return (
+                            <div className={styles.productsContainer} key={product.id}>
+                                <h2 className={styles.subTittle}>
+                                    {product.name}
+                                </h2>
+                            </div>
+                        )
+                    })}
+                </div>
+            ) : (
+                <h2 className={styles.subTittle}>There is nothing in your car...</h2>
+            )}
         </div>
     )
 }
