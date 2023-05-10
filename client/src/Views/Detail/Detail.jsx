2752b6c52d3d258eb798c212a1d82ed993df0d2b
gnzlzp <gnzlzp86@gmail.com>
2023-05-10 18:15:48 -0500

    declarando getProductById

--- client/src/Views/Detail/Detail.jsx
+++ client/src/Views/Detail/Detail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { NavLink, useParams } from "react-router-dom";
 import styles from "./Detail.module.css";
 import { useDispatch } from "react-redux";
+import { getProductById } from "../../redux/actions/actions";
 
 function Detail() {
     const {id} = useParams()
