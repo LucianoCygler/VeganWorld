86277523d9a52e375d3b582409d50fc0a0775ef5
Lucas Idiart <121633558+N3EMESIS@users.noreply.github.com>
2023-05-10 16:27:33 -0300

    Revert "se agrega url de prueba para obtener los productos y etiqueta navlink…"

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,22 +1,12 @@
-import axios from "axios";
-
 import { 
-    GET_ALL_PRODUCTS,
     ADD_CART,
     FILTER_NAME_PRODUCT, 
     FILTER_PRICE_PRODUCT,
     STATE_LOGIN
 } from "./Types/Types";
 
-const URL_MOCKY  = "https://run.mocky.io/v3/5c43f655-9150-4673-a3fe-387e9f1d03b1"
-
-export const getAllProducts = () => {
-	return async function (dispatch) {
-		const res = await axios.get(URL_MOCKY);
-        const products = res.data
-		return dispatch({ type: GET_ALL_PRODUCTS, payload: products });
-	};
-};
+// import axios from "axios";
+// const URL = "hhtp://localhost:3001";
 
 export const addCartProduct = (product) => ({
     type: ADD_CART,
