a45bf174cd8b00f3f9df6e175b118edd0a8d0571
N3EMESIS <idiartlucas@gmail.com>
2023-05-11 15:00:55 -0300

    product / products

--- client/src/Components/Products/Products.jsx
+++ client/src/Components/Products/Products.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "./Products.module.css";
+import Product from "./Product/Product";
 
-function Products () {
+function Products ({ currentItems }) {
+    const product = currentItems;
 
     return (
         <div className={styles.mainContainer}>
-
+            {product.length && product.map(({ name, id, description, stock, price, image }) => {
+                return (
+                    <div className={styles.cardContainer} key={id}>
+                        <Product id={id} name={name} description={description} stock={stock} price={price} image={image} />
+                    </div>
+                )
+            })}
         </div>
     )
 }
