81d2db13544331af74a76cd8529c26e78968d6f1
Ivan Ramos <huramos777@gmail.com>
2023-05-11 13:36:29 -0300

    revisar com funciona y terminaR

---
+++ server/src/controllers/product/filterByType.js
@@ -0,0 +1,7 @@
+const { Product } = require("../../db");
+
+const orderProductByTipo = (tipo, allProducts) => {
+  return tipo;
+};
+
+module.exports = orderProductByTipo;
---
+++ server/src/controllers/product/orderProductByName.js
@@ -0,0 +1,7 @@
+const { Product } = require("../../db");
+
+const orderProductByName = (nombre, allProducts) => {
+  return nombre;
+};
+
+module.exports = orderProductByName;
