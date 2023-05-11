b21409a808f0d074b73598d1001828dfa7000aa8
Atelok <diego135tc@gmail.com>
2023-05-11 19:28:52 -0300

    Cambios realizados en APP e Index

--- client/src/Views/index.js
+++ client/src/Views/index.js
@@ -2,10 +2,12 @@ import HomePage from "./HomePage/HomePage";
 import LandingPage from "./LandingPage/LandingPage";
 import Detail from "./Detail/Detail";
 import Cart from "./Cart/Cart";
+import Login from "./Login - Register/Login";
 
 export {
     HomePage,
     LandingPage,
     Detail,
     Cart,
+    Login,
 }
