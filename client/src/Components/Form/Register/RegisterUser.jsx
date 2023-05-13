0185966e546b447501330724e15e6b5b9bb1137a
Atelok <diego135tc@gmail.com>
2023-05-12 21:36:33 -0300

    Se realiza cambios y funcionalidades en Login y Register

--- client/src/Components/Form/Register/RegisterUser.jsx
+++ client/src/Components/Form/Register/RegisterUser.jsx
@@ -2,38 +2,37 @@ import useForm from "./UseForm/UseForm";
 import style from "./RegisterUser.module.css";
 
 const initialForm = {
-  name: "",
-  lastname: "",
+  nombre: "",
+  apellido: "",
   email: "",
-  password: "",
-  city: "",
-  direction: "",
-  telephone: "",
-  age: "",
+  contraseña: "",
+  ciudad: "",
+  direccion: "",
+  telefono: "",
+  edad: "",
   dni: "",
 };
 
 //validación con REGEX
-const validationsForm = (form) => {
+const validationsForm = (initialForm) => {
   let errors = {};
 
   const regexname = /^[a-zA-Z]+$/;
-  const regexEmail = /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i;
 
-  if (!form.name.trim()) {
-    errors.name = "El campo del nombre es requerido";
-  } else if (!regexname.test(form.name.trim()))
-    errors.name = "El campo del nombre solo acepta letras en Mayuscula y Minuscula";
+  if (!initialForm.nombre.trim()) {
+    errors.nombre = "El campo del nombre es requerido";
+  } else if (!regexname.test(initialForm.nombre.trim()))
+    errors.nombre = "El campo del nombre solo acepta letras en Mayuscula y Minuscula";
 
-  if (!form.lastname.trim()) {
-    errors.lastname = "El campo del apellido es requerido";
-  } else if (!regexEmail.test(form.image.trim()))
-    errors.lastname = "El campo del apellido solo acepta letras en Mayuscula y Minuscula";
+  if (!initialForm.apellido.trim()) {
+    errors.apellido = "El campo del apellido es requerido";
+  } else if (!regexname.test(initialForm.apellido.trim()))
+    errors.apellido = "El campo del apellido solo acepta letras en Mayuscula y Minuscula";
 
   return errors;
 };
-
 /* */
+
 
 const RegisterUser = () => {
   const { register, error, handleChange, handleBlur, handleSubmit } = useForm(
@@ -41,35 +40,37 @@ const RegisterUser = () => {
     validationsForm
   );
 
+//!borrar consolelog luego
+console.log(register);
+
+
   return (
     <form className={style.form} onSubmit={handleSubmit}>
       <div classname={style.div1}> Nombre</div>
       <div classname={style.div2}>
-        {" "}
         <input
           type="text"
-          value={register.name}
-          name="name"
+          value={register.nombre}
+          name="nombre"
           placeholder="Ingrese Nombre"
           onChange={handleChange}
           onBlur={handleBlur}
         />
       </div>
-      {error.name && <p>{error.name}</p>}
+      {error.nombre && <p>{error.nombre}</p>}
 
       <div classname={style.div3}> Apellido</div>
       <div classname={style.div4}>
-        {" "}
         <input
           type="text"
-          value={register.lastname}
-          name="lastname"
-          placeholder="Ingrese Nombre"
+          value={register.apellido}
+          name="apellido"
+          placeholder="Ingrese Apellido"
           onChange={handleChange}
           onBlur={handleBlur}
         />
       </div>
-      {error.lastname && <p>{error.lastname}</p>}
+      {error.apellido && <p>{error.apellido}</p>}
 
       <div classname={style.div5}> Correo Electronico </div>
       <div classname={style.div6}>
@@ -86,77 +87,77 @@ const RegisterUser = () => {
 
       <div classname={style.div7}> Contraseña </div>
       <div classname={style.div8}>
-        {" "}
+
         <input
           type="Password"
-          value={register.password}
-          name="password"
+          value={register.contraseña}
+          name="contraseña"
           placeholder="Ingrese una contraseña"
           onChange={handleChange}
           onBlur={handleBlur}
         />
       </div>
-      {error.password && <p>{error.password}</p>}
+      {error.contraseña && <p>{error.contraseña}</p>}
 
       <div classname={style.div9}> Ciudad</div>
       <div classname={style.div10}>
-        {" "}
+
         <input
           type="text"
-          value={register.city}
-          name="city"
+          value={register.ciudad}
+          name="ciudad"
           placeholder="Ingrese su ciudad"
           onChange={handleChange}
           onBlur={handleBlur}
         />
       </div>
-      {error.city && <p>{error.city}</p>}
+      {error.ciudad && <p>{error.ciudad}</p>}
 
       <div classname={style.div11}> Dirección</div>
       <div classname={style.div12}>
-        {" "}
+
         <input
           type="text"
-          value={register.direction}
-          name="direction"
+          value={register.direccion}
+          name="direccion"
           placeholder="Ingrese su dirección"
           onChange={handleChange}
           onBlur={handleBlur}
         />
       </div>
-      {error.direction && <p>{error.direction}</p>}
+      {error.direccion && <p>{error.direccion}</p>}
 
       <div classname={style.div13}> Telefono</div>
       <div classname={style.div14}>
-        {" "}
+
         <input
           type="text"
-          value={register.telephone}
-          name="telephone"
+          value={register.telefono}
+          name="telefono"
           placeholder="Ingrese su teléfono"
           onChange={handleChange}
           onBlur={handleBlur}
         />
       </div>
-      {error.telephone && <p>{error.telephone}</p>}
+      {error.telefono && <p>{error.telefono}</p>}
 
       <div classname={style.div15}> Edad</div>
       <div classname={style.div16}>
-        {" "}
+
         <input
           type="text"
-          value={register.age}
-          name="age"
+          value={register.edad}
+          name="edad"
           placeholder="Ingrese edad"
           onChange={handleChange}
           onBlur={handleBlur}
         />
       </div>
-      {error.age && <p>{error.age}</p>}
+      {error.edad && <p>{error.edad}</p>}
 
       <div classname={style.div17}> DNI</div>
       <div classname={style.div18}>
-        {" "}
+    
         <input
           type="text"
           value={register.dni}
@@ -169,7 +170,7 @@ const RegisterUser = () => {
       {error.dni && <p>{error.dni}</p>}
 
       <div classname={style.div19}>
-        <button>Sing In</button>{" "}
+        <button>Sing In</button>
       </div>
     </form>
   );
