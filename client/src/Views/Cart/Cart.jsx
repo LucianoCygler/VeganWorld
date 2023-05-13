d06d566728f20271ab00a51e3de6b97a98544687
Luciano Cygler <lucianocygler@outlook.com>
2023-05-13 19:05:21 -0300

    implementacion de la sesion de usuario para carrito y contabilizacion de produtos desde back

--- client/src/Views/Cart/Cart.jsx
+++ client/src/Views/Cart/Cart.jsx
@@ -17,57 +17,58 @@ import { NavLink } from "react-router-dom";
 
 function Cart() {
   const cart = useSelector((state) => state.cart);
+  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
-	const handleClick = (event) => {
-		const name = event.target.name;
-		const id = event.target.value;
-		switch (name) {
-			case "clear":
-				return dispatch(dropProduct(id));
-			case "play":
-				return alert("ir al metodo de pago");
-			case "postOrder":
-				let importeTotal = 0;
-				cart.forEach((product) => (importeTotal += product.importe));
-				const order = {
-					cliente_id: 1,
-					importe: importeTotal,
-					productos: cart.map((product) => product.id),
-				};
-				Pop_up(
-					"success",
-					"Order Ceated",
-					"You can find your orders in MyOrders!"
-				);
-				return dispatch(createOrder(order));
-			// case "increment":
-			// 	return dispatch(incrementProduct(id));
-			// case "decrement":
-			// 	return dispatch(decrementProduct(id));
-			default:
-				return;
-		}
-	};
+  const handleClick = (event) => {
+    const name = event.target.name;
+    const id = event.target.value;
+    switch (name) {
+      case "clear":
+        return dispatch(dropProduct(id));
+      case "play":
+        return alert("ir al metodo de pago");
+      case "postOrder":
+        let importeTotal = 0;
+        cart.forEach((product) => (importeTotal += product.importe));
+        const order = {
+          cliente_id: user?.id,
+          importe: importeTotal,
+          productos: cart.map((product) => product.id),
+        };
+        Pop_up(
+          "success",
+          "Order Ceated",
+          "You can find your orders in MyOrders!"
+        );
+        return dispatch(createOrder(order));
+      // case "increment":
+      // 	return dispatch(incrementProduct(id));
+      // case "decrement":
+      // 	return dispatch(decrementProduct(id));
+      default:
+        return;
+    }
+  };
 
-	return (
-		<div className={styles.mainContainer}>
-			<div className={styles.tittle}>
-				<h1>Cart</h1>
-			</div>
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.tittle}>
+        <h1>Cart</h1>
+      </div>
 
-			{cart.length > 0 ? (
-				// <div className={styles.container}>
-				<>
-					{cart.map((product, index) => {
-						return (
-							<>
-								<div
-									className={styles.productsContainer}
-									key={index}
-									style={{ gridRow: `${index + 1}` }}
-								>
-									{/*<div>
+      {cart.length > 0 ? (
+        // <div className={styles.container}>
+        <>
+          {cart.map((product, index) => {
+            return (
+              <>
+                <div
+                  className={styles.productsContainer}
+                  key={index}
+                  style={{ gridRow: `${index + 1}` }}
+                >
+                  {/*<div>
 
 										<button name="increment" onClick={handleClick}>
 											+
@@ -104,48 +105,49 @@ function Cart() {
                     </p>
                   </div>
 
-									<div className={styles.delete}>
-									<button
-										>
-											<FontAwesomeIcon icon={faRecycle} name="clear"
-											value={product.id}
-											onClick={handleClick} />
-										</button>
-									</div>
-									<div>
-										<NavLink>
-											<p>Edit</p>
-										</NavLink>
-									</div>
-								</div>
-							</>
-						);
-					})}
-					<div className={styles.orderSumary}>
-						<div className={styles.subtotal}>
-							<h4>suma de precio de productos</h4>
-						</div>
-						<div className={styles.titleOrder}>
-							<h2>Order Sumary</h2>
-						</div>
-						<div className={styles.btnOrder}>
-							<button onClick={handleClick} name="pay">
+                  <div className={styles.delete}>
+                    <button>
+                      <FontAwesomeIcon
+                        icon={faRecycle}
+                        name="clear"
+                        value={product.id}
+                        onClick={handleClick}
+                      />
+                    </button>
+                  </div>
+                  <div>
+                    <NavLink>
+                      <p>Edit</p>
+                    </NavLink>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+          <div className={styles.orderSumary}>
+            <div className={styles.subtotal}>
+              <h4>suma de precio de productos</h4>
+            </div>
+            <div className={styles.titleOrder}>
+              <h2>Order Sumary</h2>
+            </div>
+            <div className={styles.btnOrder}>
+              {/* <button onClick={handleClick} name="pay">
 								Pagar
-							</button>
-							<button onClick={handleClick} name="postOrder">
-								Crear orden
-							</button>
-						</div>
-						<div className={styles.orderTotal}></div>
-					</div>
-				</>
-			) : (
-				// </div>
-				<h2 className={styles.subTittle}>There is nothing in your car...</h2>
-			)}
-		</div>
-	);
-
+							</button> */}
+              <button onClick={handleClick} name="postOrder">
+                Crear orden
+              </button>
+            </div>
+            <div className={styles.orderTotal}></div>
+          </div>
+        </>
+      ) : (
+        // </div>
+        <h2 className={styles.subTittle}>There is nothing in your car...</h2>
+      )}
+    </div>
+  );
 }
 
 export default Cart;
