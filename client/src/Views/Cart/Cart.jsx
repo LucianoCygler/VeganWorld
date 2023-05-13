a736a37aae4e1d3faff985a4666ab76697e47e9e
gnzlzp <gnzlzp86@gmail.com>
2023-05-13 14:57:12 -0500

    mas estilo de cart

--- client/src/Views/Cart/Cart.jsx
+++ client/src/Views/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./Cart.module.css";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faRecycle } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
 import {
 	createOrder,
@@ -8,6 +10,8 @@ import {
 	dropProduct,
 	incrementProduct,
 } from "../../redux/actions/actions";
+import Pop_up from "../../Utils/Pop_up/Pop_up";
+import { NavLink } from "react-router-dom";
 
 function Cart() {
 	const cart = useSelector((state) => state.cart);
@@ -29,24 +33,11 @@ function Cart() {
 					importe: importeTotal,
 					productos: cart.map((product) => product.id),
 				};
-				
-				const Toast = Swal.mixin({
-          toast: true,
-          position: 'top-end',
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-          }
-        })
-        
-        Toast.fire({
-          icon: 'success',
-          title: 'Product added',
-          text: "You can find your orders in MyOrders!",
-        })
+				Pop_up(
+					"success",
+					"Order Ceated",
+					"You can find your orders in MyOrders!"
+				);
 				return dispatch(createOrder(order));
 			// case "increment":
 			// 	return dispatch(incrementProduct(id));
@@ -60,18 +51,20 @@ function Cart() {
 	return (
 		<div className={styles.mainContainer}>
 			<div className={styles.tittle}>
-			<h1 >Cart</h1>
-
+				<h1>Cart</h1>
 			</div>
 
 			{cart.length > 0 ? (
 				// <div className={styles.container}>
 				<>
-					
 					{cart.map((product, index) => {
 						return (
 							<>
-								<div className={styles.productsContainer} key={index} style={{ gridRow: `${index + 1}` }}>
+								<div
+									className={styles.productsContainer}
+									key={index}
+									style={{ gridRow: `${index + 1}` }}
+								>
 									{/*<div>
 										<button name="increment" onClick={handleClick}>
 											+
@@ -109,41 +102,44 @@ function Cart() {
 									</div>
 
 									<div className={styles.delete}>
-										<button
-											name="clear"
-											value={product.id}
-											onClick={handleClick}
+									<button
 										>
-											X
+											<FontAwesomeIcon icon={faRecycle} name="clear"
+											value={product.id}
+											onClick={handleClick} />
 										</button>
-										
+									</div>
+									<div>
+										<NavLink>
+											<p>Edit</p>
+										</NavLink>
 									</div>
 								</div>
 							</>
 						);
 					})}
+					<div className={styles.orderSumary}>
+						<div className={styles.subtotal}>
+							<h4>suma de precio de productos</h4>
+						</div>
+						<div className={styles.titleOrder}>
+							<h2>Order Sumary</h2>
+						</div>
+						<div className={styles.btnOrder}>
+							<button onClick={handleClick} name="pay">
+								Pagar
+							</button>
+							<button onClick={handleClick} name="postOrder">
+								Crear orden
+							</button>
+						</div>
+						<div className={styles.orderTotal}></div>
+					</div>
 				</>
 			) : (
 				// </div>
 				<h2 className={styles.subTittle}>There is nothing in your car...</h2>
-				)}
-			<div className={styles.orderSumary}>
-				<div className={styles.subtotal}>
-					<h4>suma de precio de productos</h4>
-				</div>
-				<div className={styles.titleOrder}>
-					<h2>Order Sumary</h2>
-				</div>
-				<div className={styles.btnOrder}>
-				<button onClick={handleClick} name="pay">
-						Pagar
-					</button>
-					<button onClick={handleClick} name="postOrder">
-						Crear orden
-					</button>
-				</div>
-				<div className={styles.orderTotal}></div>
-			</div>
+			)}
 		</div>
 	);
 }
--- client/src/Views/Detail/Detail.jsx
+++ client/src/Views/Detail/Detail.jsx
@@ -1,87 +1,91 @@
 import React, { useEffect, useState } from "react";
+import { Ring } from "@uiball/loaders";
 import styles from "./Detail.module.css";
 import { NavLink, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  getProductById,
-  cleanDetail,
-  addCartProduct,
+	getProductById,
+	cleanDetail,
+	addCartProduct,
 } from "../../redux/actions/actions";
 import Pop_up from "../../Utils/Pop_up/Pop_up";
 
 function Detail() {
-  const { id } = useParams();
+	const { id } = useParams();
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const [product] = useSelector((state) => state.product);
+	const [product] = useSelector((state) => state.product);
 
-  const [quantity, setQuantity] = useState(1);
+	const [quantity, setQuantity] = useState(1);
 
-  const handleClick = () => {
-   try {
-    dispatch(addCartProduct(product, quantity));
-    Pop_up("success", "Product added", "You can find your products in Cart!")
-  } catch ({message}) {
-    Pop_up("info", "Product added", message )
-  }};
+	const handleClick = () => {
+		try {
+			dispatch(addCartProduct(product, quantity));
+			Pop_up("success", "Product added", "You can find your products in Cart!");
+		} catch ({ message }) {
+			Pop_up("info", "Product added", message);
+		}
+	};
 
-  const handleChange = (event) => setQuantity(event.target.value);
+	const handleChange = (event) => setQuantity(event.target.value);
 
-  useEffect(() => {
-    dispatch(getProductById(id));
-    return () => dispatch(cleanDetail());
-  }, [id]);
+	useEffect(() => {
+		dispatch(getProductById(id));
+		return () => dispatch(cleanDetail());
+	}, [id]);
 
-  return (
-    <>
-      {product?.nombre ? (
-        <div
-          className={styles.mainContainer}
-          style={{ backgroundColor: "rgba(42, 66, 49, 0.5)" }}
-        >
-          <h1>{product.nombre}</h1>
-          <img
-            src={product.imagen}
-            alt={product.nombre}
-            style={{ maxWidth: "300px" }}
-          />
+	return (
+		<>
+			{product?.nombre ? (
+				<div
+					className={styles.mainContainer}
+					// style={{ backgroundColor: "rgba(42, 66, 49, 0.5)" }}
+				>
+					<h1>{product.nombre}</h1>
+					<img
+						src={product.imagen}
+						alt={product.nombre}
+						style={{ maxWidth: "300px" }}
+					/>
 
-          <div>&#11088;&#11088;&#11088;&#11088;&#11088;</div>
-          <p>{product.descripcion}</p>
-          <div>
-            Precio: <span>${product.precio}</span>
-          </div>
-          {/* <div>Precio al mayor:</div> */}
-          <div>
-            <span></span>
-          </div>
-          <div></div>
-          <NavLink to="/" className={styles.link}>
-            <button>Atras</button>
-          </NavLink>
-          <NavLink to={"/Cart"}>
-            <button>Ir al carrito</button>
-          </NavLink>
+					<div>&#11088;&#11088;&#11088;&#11088;&#11088;</div>
+					<p>{product.descripcion}</p>
+					<div>
+						Precio: <span>${product.precio}</span>
+					</div>
+					{/* <div>Precio al mayor:</div> */}
+					<div>
+						<span></span>
+					</div>
+					<div></div>
+					<NavLink to="/" className={styles.link}>
+						<button>Atras</button>
+					</NavLink>
+					<NavLink to={"/Cart"}>
+						<button>Ir al carrito</button>
+					</NavLink>
 
-          <input
-            type="number"
-            name="quantity"
-            id=""
-            style={{ width: "3rem" }}
-            value={quantity}
-            min={1}
-            max={10}
-            onChange={handleChange}
-          />
+					<input
+						type="number"
+						name="quantity"
+						id=""
+						style={{ width: "3rem" }}
+						value={quantity}
+						min={1}
+						max={10}
+						onChange={handleChange}
+					/>
 
-          <button onClick={handleClick}>Añade al carrito</button>
+					<button onClick={handleClick}>Add to cart</button>
+				</div>
+			) : (
+        <div style={{paddingTop:"150px"}}>
+          <Ring size={200} lineWeight={5} speed={2} color="rgba(29, 103, 88, 0.6)" />
         </div>
-      ) : (
-        <h1>LOADING...</h1>
-      )}
-    </>
-  );
+			)}
+		</>
+	);
 }
 
 export default Detail;
