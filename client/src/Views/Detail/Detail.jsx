0fc8c69c193165dc0e6d32f7e02a34f82d371240
N3EMESIS <idiartlucas@gmail.com>
2023-05-13 23:58:36 -0300

    Detail CSS parejo y basico / cambios en el input

--- client/src/Views/Detail/Detail.jsx
+++ client/src/Views/Detail/Detail.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Ring } from "@uiball/loaders";
 import styles from "./Detail.module.css";
-import { NavLink, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {
-	getProductById,
-	cleanDetail,
-	addCartProduct,
-} from "../../redux/actions/actions";
+import { getProductById, cleanDetail, addCartProduct } from "../../redux/actions/actions";
 import Pop_up from "../../Utils/Pop_up/Pop_up";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Detail() {
 	const { id } = useParams();
@@ -28,7 +26,15 @@ function Detail() {
 		}
 	};
 
-	const handleChange = (event) => setQuantity(event.target.value);
+	const handleDecrement = () => {
+		setQuantity(quantity > 1 ? quantity - 1 : 1);
+	}
+
+	const handleIncrement = () => {
+		if(quantity < 10){
+			setQuantity(quantity + 1);
+		}
+	}
 
 	useEffect(() => {
 		dispatch(getProductById(id));
@@ -37,48 +43,39 @@ function Detail() {
 
 	return (
 		<div className={styles.mainContainer}>
-			{product?.nombre ? (
 			<div className={styles.container}>
-				<div className={styles.divLink}>
-					<NavLink to="/" className={styles.linkBack}>
-						<button className={styles.buttonBack}>Atras</button>
-					</NavLink>
+				<div className={styles.linkContainer}>
+					<Link to="/" className={styles.linkBack}>
+						<FontAwesomeIcon icon={faArrowLeftLong} />	
+					</Link>
 				</div>
-				<img src={product.imagen} alt={product.nombre} style={{ maxWidth: "300px" }} className={styles.image}/>
-				<div className={styles.descriptionDiv}>
-					<h1>{product.nombre}</h1>
-					<p className={styles.p}>{product.descripcion}</p>
-					<div className={styles.priceDiv}>
-						Precio: <span className={styles.spanPrice}>${product.precio}</span>
+				{product?.nombre ? (
+					<div className={styles.descriptionDiv}>
+						<div className={styles.imageDiv}>
+							<img src={product.imagen} alt={product.nombre} className={styles.image}/>
+						</div>
+						<div className={styles.detailDiv}>
+							<h1 className={styles.h1}>{product.nombre}</h1>
+							<h2>★★★★</h2>
+							<h3 className={styles.p}>{product.descripcion}</h3>
+							<div className={styles.priceDiv}>
+								<h2 className={styles.price}>Precio: <span className={styles.spanPrice}>${product.precio}</span></h2>
+							</div>
+							<div className={styles.productValue}>
+								<h2>Cantidad:</h2>
+								<h3 className={styles.quantitySpan}>{quantity}</h3>
+								<button className={styles.quantityButton} onClick={handleDecrement}>-</button>
+								<button className={styles.quantityButton} onClick={handleIncrement}>+</button>
+								<button className={styles.button} onClick={handleClick}>Add to cart</button>
+							</div>
+						</div>
 					</div>
-						{/* <div>REVIEW:</div> */}
-					<div>
-						<span></span>
-					</div>
-					<div></div>
-					{/* <NavLink to={"/Cart"}>
-						<button className={styles.button}>Ir al carrito</button>
-					</NavLink> */}
-					<input
-						type="number"
-						name="quantity"
-						id=""
-						style={{ width: "3rem" }}
-						value={quantity}
-						min={1}
-						max={10}
-						onChange={handleChange}
-						className={styles.input}
-					/>
-
-					<button className={styles.button} onClick={handleClick}>Add to cart</button>
-				</div>
+				) : (
+					<div style={{paddingTop:"150px"}}>
+          				<Ring size={200} lineWeight={5} speed={2} color="rgba(29, 103, 88, 0.6)" />
+        			</div>
+				)}
 			</div>
-			) : (
-        	<div className={styles.alertDiv} style={{paddingTop:"150px"}}>
-          		<Ring className={styles.alert} size={200} lineWeight={5} speed={2} color="rgba(29, 103, 88, 0.6)" />
-        	</div>
-			)}
 		</div>
 	);
 }
