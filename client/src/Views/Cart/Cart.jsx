9e9496d274182f76a4ed8e7b3e6b109b77dcdcdd
gnzlzp <gnzlzp86@gmail.com>
2023-05-14 15:45:26 -0500

    agrego boton delete

--- client/src/Views/Cart/Cart.jsx
+++ client/src/Views/Cart/Cart.jsx
@@ -146,15 +146,24 @@ function Cart() {
 
 									<div className={styles.delete}>
 										<button
-											className={styles.btnDelete}
+											className={styles.btn}
 											name="delete"
 											value={product.id}
 											onClick={handleClick}
 										>
-											<div className={styles.icon}>
-												<FontAwesomeIcon icon={faRecycle} spin size="2xl" />
-											</div>
-
+											<svg
+												viewBox="0 0 15 17.5"
+												height="17.5"
+												width="15"
+												xmlns="http://www.w3.org/2000/svg"
+												className={styles.icon}
+											>
+												<path
+													transform="translate(-2.5 -1.25)"
+													d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z"
+													id="Fill"
+												></path>
+											</svg>
 										</button>
 									</div>
 								</div>
@@ -184,7 +193,7 @@ function Cart() {
 									Generate order
 								</button>
 							) : (
-								<NavLink to={"/Login"}>
+								<NavLink to={"/login"}>
 									<p>Login</p>
 								</NavLink>
 							)}
