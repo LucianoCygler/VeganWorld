4ddef40ebd1139abc35633de0131bd953aa4532f
ngaldos <ngaldos98@gmail.com>
2023-05-14 18:04:37 -0300

    about y contactUs

--- client/src/Views/ContactUs/ContactUs.jsx
+++ client/src/Views/ContactUs/ContactUs.jsx
@@ -2,8 +2,11 @@ import styles from "./ContactUs.module.css";
 const ContactUs = () => {
   return (
     <div className={styles.contenedor}>
-      {" "}
-      <h1>Aca van datos de contacto / form para enviar mail </h1>
+      <h2>Contact us form our media for customer support: </h2>
+      <div>
+        <h3>Phone: +542219876543</h3>
+        <h3>E-mail: info@veganworld.com</h3>
+      </div>
     </div>
   );
 };
