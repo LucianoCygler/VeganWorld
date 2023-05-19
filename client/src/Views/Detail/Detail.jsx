6d7e42be56ec57d0d5ab620c9b003e4ff4aa7f6c
Luciano Cygler <lucianocygler@outlook.com>
2023-05-19 17:50:28 -0300

    Avance de detalle de producto

--- client/src/Views/Detail/Detail.jsx
+++ client/src/Views/Detail/Detail.jsx
@@ -74,7 +74,7 @@ import {
   PopoverArrow,
   PopoverCloseButton,
 } from "@chakra-ui/react";
-
+import "./Detail.css";
 function Detail() {
   const { id } = useParams();
   const navigate = useNavigate();
@@ -210,8 +210,12 @@ function Detail() {
                     <h1>${product.precio}</h1>
                     <p>Cantidad: {quantity}</p>
                   </Grid>
-                  <Button onClick={handleDecrement}>-</Button>
-                  <Button onClick={handleIncrement}>+</Button>
+                  <Button onClick={handleDecrement} marginRight="1em">
+                    -
+                  </Button>
+                  <Button onClick={handleIncrement} marginRight="1em">
+                    +
+                  </Button>
                   <Button
                     isLoading={loading}
                     colorScheme="teal"
@@ -232,15 +236,45 @@ function Detail() {
           color="rgba(29, 103, 88, 0.6)"
         />
       )}
-      <Box>
+      <Box
+        w="80%"
+        bg="white  "
+        paddingTop="2em"
+        margin="auto"
+        shadow="0px 4px 10px rgba(0, 0, 0, 0.2)"
+      >
         {productReviews
           ? productReviews.slice(0, 3).map((review) => {
               return (
                 <>
-                  <h1>{review.titulo}</h1>
-                  <span>{review.cliente_nombre}</span>
-                  <p>{review.descripcion}</p>
-                  <p>{review.fecha}</p>
+                  <Box display="inline-block" marginRight="1.5em">
+                    <div class="cardReview">
+                      <div class="header">
+                        <div class="image"></div>
+                        <div>
+                          <div class="stars">
+                            {Array.from({ length: review.estrellas }).map(
+                              (_, index) => (
+                                <svg
+                                  key={index}
+                                  fill="currentColor"
+                                  viewBox="0 0 20 20"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                </svg>
+                              )
+                            )}
+                          </div>
+                          <p class="name">{review.cliente_nombre}</p>
+                          <p>{review.titulo}</p>
+                        </div>
+                      </div>
+
+                      <p class="message">{review.descripcion}</p>
+                      <small class="message">{review.fecha}</small>
+                    </div>
+                  </Box>
                 </>
               );
             })
