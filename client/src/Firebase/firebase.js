d063d272eacb56654490c542d8488f7ac50aefee
Ivan Ramos <huramos777@gmail.com>
2023-05-24 18:40:32 -0300

    sign up validations

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCLNsFsX6qUg-NNZ084fjZpkPa5SHu2QZk",
-  authDomain: "veganworld-2aef9.firebaseapp.com",
-  projectId: "veganworld-2aef9",
-  storageBucket: "veganworld-2aef9.appspot.com",
-  messagingSenderId: "700564277416",
-  appId: "1:700564277416:web:244cd35953fb18b08d5a9d",
-};
+import firebaseConfig from "./firebaseConfig";
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
