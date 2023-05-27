10cb7f9e1ac016e3c3178c68d2cb4dd9aa50e46a
Luciano Cygler <lucianocygler@outlook.com>
2023-05-27 17:16:21 -0300

    ok

--- client/src/Graficos/GraficoUsuarios.jsx
+++ client/src/Graficos/GraficoUsuarios.jsx
@@ -6,49 +6,60 @@ import {
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 import { faker } from "@faker-js/faker";
-
+//.
 ChartJs.register(
-    CategoryScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-)
+  CategoryScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-export const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+export const labels = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 export const optionsLine = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top'
-      },
-      title: {
-        display: true,
-        text: 'Users register'
-      }
-    }
-  };
-  
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Users register",
+    },
+  },
+};
+
 export const dataLine = {
-    labels,
-    datasets: [
-      {
-        label: 'Users',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      }
-    ]
-  };
+  labels,
+  datasets: [
+    {
+      label: "Users",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
+};
 
 export default function GraficoUsuarios() {
-    return (
-        <Line options={optionsLine} data={dataLine} />
-    )
+  return <Line options={optionsLine} data={dataLine} />;
 }
