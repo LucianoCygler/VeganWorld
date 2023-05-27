00b9779aacdf8fd0983db667ff6749143ea7200c
Atelok <diego135tc@gmail.com>
2023-05-26 22:08:09 -0300

    Cambios en las reviews

--- client/src/Components/Dashboard/Reviews.js
+++ client/src/Components/Dashboard/Reviews.js
@@ -3,28 +3,33 @@ import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllReviews } from "../../redux/actions/actions";
+import { getAllReviews, getAllProducts } from "../../redux/actions/actions";
 
 function ReviewsDashboard() {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(getAllReviews());
+		dispatch(getAllProducts());
 	}, []);
 
+	const products = useSelector((state)=> state.products)
 	const allReviews = useSelector((state) => state.allReviews);
+	
+
 	const MapAllReviews = allReviews.map((rev, index) => {
+		const MapProduct = products.filter((product)=> Number(product.id) === rev.ProductId)
+		
 		return {
-      id: rev.id,
+      		id: rev.id,
 			Name: rev.cliente_nombre,
 			Title: rev.titulo,
 			Description: rev.descripcion,
 			Rate: rev.estrellas,
 			Date: rev.fecha,
-			Product: rev.product_id,
+			Product: MapProduct[0].nombre,
 		};
 	});
-  console.log(allReviews);
 	return (
 		<Box sx={{ height: 250, width: "100%" }}>
 			<DataGrid
