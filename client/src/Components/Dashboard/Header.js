423e186ca15c12945324a7b38f91a071224f2f2e
gnzlzp <gnzlzp86@gmail.com>
2023-05-26 19:29:20 -0500

    titulo

--- client/src/Components/Dashboard/Header.js
+++ client/src/Components/Dashboard/Header.js
@@ -79,7 +79,7 @@ function Header(props) {
 					<Grid container alignItems="center" spacing={1}>
 						<Grid item xs>
 							<Typography color="inherit" variant="h5" component="h1">
-								Authentication
+								Dashboard
 							</Typography>
 						</Grid>
 					</Grid>
