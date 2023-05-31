fbe79968b5297532bc49de606cef04ce8bc983b0
Luciano Cygler <lucianocygler@outlook.com>
2023-05-31 16:53:39 -0300

    ok

--- client/src/Components/Dashboard/Navigator.js
+++ client/src/Components/Dashboard/Navigator.js
@@ -17,69 +17,76 @@ import { useDispatch } from "react-redux";
 import { ChangeLabel } from "../../redux/actions/actions";
 
 export default function Navigator(props) {
+  const categories = [
+    {
+      id: "Graph",
+      icon: <LeaderboardIcon />,
+      active: true,
+    },
+    { id: "Clients", icon: <PeopleIcon />, active: false },
+    { id: "Products", icon: <DnsRoundedIcon />, active: false },
+    { id: "Reviews", icon: <ReviewsIcon />, active: false },
+    { id: "Orders", icon: <WarehouseIcon />, active: false },
+    { id: "OrderTest", icon: <WarehouseIcon />, active: false },
+  ];
 
-	const categories = [
-		{
-			id: "Graph",
-			icon: <LeaderboardIcon />,
-			active: true,
-		},
-		{ id: "Clients", icon: <PeopleIcon />, active: false },
-		{ id: "Products", icon: <DnsRoundedIcon />, active: false },
-		{ id: "Reviews", icon: <ReviewsIcon />, active: false },
-		{ id: "Orders", icon: <WarehouseIcon />, active: false },
-	];
+  const item = {
+    py: "2px",
+    px: 3,
+    color: "rgba(255, 255, 255, 0.7)",
+    "&:hover, &:focus": {
+      bgcolor: "rgba(255, 255, 255, 0.08)",
+    },
+  };
 
-	const item = {
-		py: "2px",
-		px: 3,
-		color: "rgba(255, 255, 255, 0.7)",
-		"&:hover, &:focus": {
-			bgcolor: "rgba(255, 255, 255, 0.08)",
-		},
-	};
+  const itemCategory = {
+    boxShadow: "0 -1px 0 rgb(255,255,255,0.1) inset",
+    py: 1.5,
+    px: 3,
+  };
+  const { ...other } = props;
+  const dispatch = useDispatch();
 
-	const itemCategory = {
-		boxShadow: "0 -1px 0 rgb(255,255,255,0.1) inset",
-		py: 1.5,
-		px: 3,
-	};
-	const { ...other } = props;
-	const dispatch = useDispatch();
+  const [labels, setLabels] = useState("");
 
-	const [labels, setLabels] = useState("");
+  const handleLabel = (id) => {
+    dispatch(ChangeLabel(id));
+    setLabels(id);
+  };
 
-	const handleLabel = (id) => {
-		dispatch(ChangeLabel(id));
-		setLabels(id);
-	};
-
-	return (
-		<>
-			<Drawer {...other}  >
-				<List disablePadding sx={{bgcolor:"#1d5c63"}}>
-					<ListItem
-						sx={{ ...item, ...itemCategory, fontSize: 22, fontWeight:"bold", color: "#fff", bgcolor: "#1d5c63" }}
-					>
-						VeganWorld
-					</ListItem>
-					{categories.map(({ id, icon }) => (
-						<Box key={id} sx={{ bgcolor: "#319795" }}>
-							<ListItem disablePadding key={id}>
-								<ListItemButton
-									selected={labels === id ? true : false}
-									sx={item}
-									onClick={() => handleLabel(id)}
-								>
-									<ListItemIcon>{icon}</ListItemIcon>
-									<ListItemText>{id}</ListItemText>
-								</ListItemButton>
-							</ListItem>
-							<Divider sx={{ mt: 2 }} />
-						</Box>
-					))}
-				</List>
-			</Drawer>
-		</>
-	);
+  return (
+    <>
+      <Drawer {...other}>
+        <List disablePadding sx={{ bgcolor: "#1d5c63" }}>
+          <ListItem
+            sx={{
+              ...item,
+              ...itemCategory,
+              fontSize: 22,
+              fontWeight: "bold",
+              color: "#fff",
+              bgcolor: "#1d5c63",
+            }}
+          >
+            VeganWorld
+          </ListItem>
+          {categories.map(({ id, icon }) => (
+            <Box key={id} sx={{ bgcolor: "#319795" }}>
+              <ListItem disablePadding key={id}>
+                <ListItemButton
+                  selected={labels === id ? true : false}
+                  sx={item}
+                  onClick={() => handleLabel(id)}
+                >
+                  <ListItemIcon>{icon}</ListItemIcon>
+                  <ListItemText>{id}</ListItemText>
+                </ListItemButton>
+              </ListItem>
+              <Divider sx={{ mt: 2 }} />
+            </Box>
+          ))}
+        </List>
+      </Drawer>
+    </>
+  );
 }
